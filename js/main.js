be52d054d90b49ec4a96e4e1d6ba2295b61b60ff
Mikhil Jose <mj4study@gmail.com>
2017-06-27 17:24:52 +0530

    transition initialization added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
 function barba_main () {
+    
+    /*Barba main initialization*/
 	window.onload = function() {
   		Barba.Pjax.start();
     }
+    
+    
+    /*prevent same page reload*/
+    var links = document.querySelectorAll('a[href]');
+    var cbk = function(e) {
+     if(e.currentTarget.href === window.location.href) {
+       e.preventDefault();
+       e.stopPropagation();
+     }
+    };
+    for(var i = 0; i < links.length; i++) {
+      links[i].addEventListener('click', cbk);
+    }
+    
+    
+    
+    /*Page transtion initialization*/
+    Barba.Pjax.getTransition = function() {
+      return Transition;
+    };
+
+    
 }
 
 
@@ -9,6 +33,17 @@ function barba_main () {
 
 
 barba_main();
+
+
+
+
+
+
+
+
+
+
+
 
 
 
